b3b64e4fa88efe4c1f4fe6b47a3ace60e675eb10
frank3b <elverb@gmail.com>
2013-11-20 15:15:31 -0500

    Proyecto Inicial

---
+++ js/vinculaciones.js
@@ -0,0 +1,177 @@
+/**
+ * 
+ */
+
+function validarIngreso(){	
+	$.mobile.loading('show');
+	
+	var usuario = $('#usuario').val();
+    var password = $('#password').val();
+    
+    if(usuario != null && password != null){
+        
+        var query = new Kinvey.Query();
+        query.equalTo('login', usuario);
+        Kinvey.DataStore.find('USUARIOS', query, {
+            success: function(response) {
+                
+                if(password == response[0].password){
+                    $.mobile.changePage("#vinculacion", {
+                            transition: "pop",
+                            reverse: false,
+                	        changeHash: false
+                	});
+                    
+                    var txtBienv = "Bienvenido: " + response[0].nombre;
+    				$('#bienvenidaText').text(txtBienv);
+                } else {
+                    $('#mensaje').show();
+                    $('#mensaje').text( 'El nombre de usuario o la contrase\u00F1a introducidos no son correctos.' );
+                }                
+                $.mobile.loading('hide');
+            },
+            error: function(error){
+    			console.log(error);
+				$('#mensaje').show();
+    	        $('#mensaje').text( 'El nombre de usuario o la contrase\u00F1a introducidos no son correctos.' );
+    	        $.mobile.loading('hide');
+			}
+        });
+       
+        
+	} else {
+		$('#mensaje').show();
+		$('#mensaje').text( 'Debe ingresar el nombre de usuario y la contrase\u00F1a.' );
+		$.mobile.loading('hide');
+	}  
+	
+}
+
+function salir(){
+	$("#mensaje").hide();
+	$("#mensajeVinculacion").hide();
+	
+	$('#mensaje').text("");
+	$('#mensajeVinculacion').text("");
+	
+	$('#usuario').val("");
+	$('#password').val("");
+	
+}
+
+function guardar(){
+	$('#mensajeVinculacion').show();
+	$('#mensajeVinculacion').addClass('success');
+	$('#mensajeVinculacion').text( 'La informaci\u00F3n se ha almacenado correctamente.' );
+}
+
+function segmentar(){
+	$('#mensajeVinculacion').show();
+	$('#mensajeVinculacion').addClass('success');
+	$('#mensajeVinculacion').text('El proceso de segmentaci\u00F3n termino exitosamente.');
+	
+	$('#segmento').val("PERSONAL PLUS");
+	$('#subsegmento').val("Basico");
+	$('#tamanoComercial').val("0.00");
+	
+	$('#identificacionDiv').trigger('collapse');
+	$('#ubicacion').trigger('collapse');
+	$('#datosFinancieros').trigger('collapse');
+	$('#determinarCiiuButton').trigger('collapse');
+	$('#datosGenerales').trigger('expand');
+	$('#segmento').trigger("focus");
+}
+
+function validarListasControl(){
+	$('#mensajeVinculacion').show();
+	$('#mensajeVinculacion').addClass('info');
+	$('#mensajeVinculacion').text('No se encuentra en listas de control.');
+}
+
+function vincular(){
+	$('#mensajeVinculacion').show();
+	$('#mensajeVinculacion').addClass('info');
+	$('#mensajeVinculacion').text('Vinculaci\u00F3n ejecutada exitosamente.');
+}
+
+function consultarVinculados() {
+	
+	var data = getViculados();
+	console.log("getViculados...");
+	if(data != null){
+		
+		var clientes = data.respuesta.clientes;
+		
+		$.each(clientes, function(index, obj) {
+			console.log("iterando cliente: " + obj.cliente.nombre);
+			var item = "<li><a href=\"#vinculacion\"> "+
+			"<h3>" + obj.cliente.nombre +"</h3>" +
+			"<p><strong>Cedula:</strong> "+ obj.cliente.cedula +"</p>" +
+			"<p class=\"ui-li-aside\"><strong>"+ obj.cliente.estado +"</strong></p>" +
+			"</a></li>";
+			
+			
+			$("#customerList").append(item);
+            //$("#customerList").listview("refresh");
+		});
+		
+	}
+	
+	
+}
+
+function getViculados() {
+	var json = {
+		"respuesta" : {
+			"clientes" : [ {
+				"cliente" : {
+					"nombre" : "Frank Bedoya",
+					"estado" : "Pendiente",
+					"cedula" : "66576512"
+				}
+			}/*, {
+				"cliente" : {
+					"nombre" : "Avery Walke",
+					"estado" : "Activo",
+					"cedula" : "98798234"
+				}
+			}, {
+				"cliente" : {
+					"nombre" : "Walter White",
+					"estado" : "Pendiente",
+					"cedula" : "76009384"
+				}
+			}, {
+				"cliente" : {
+					"nombre" : "Dexter Morgan",
+					"estado" : "Rechazado",
+					"cedula" : "1235463"
+				}
+			}, {
+				"cliente" : {
+					"nombre" : "Stephen Weber",
+					"estado" : "Activo",
+					"cedula" : "872348398"
+				}
+			}*/ ]
+		}
+
+	};
+	
+	return json;
+
+}
+
+function iniciarCampos(){
+	
+	$("#mensaje").hide();
+	$("#mensajeVinculacion").hide();
+	
+	$("#segmento").attr('readonly', true);
+	$("#subsegmento").attr('readonly', true);
+	$("#tamanoComercial").attr('readonly', true);
+	$("#llaveCRM").attr('readonly', true);
+	
+	$('#resetButton').closest('.ui-btn').hide();
+	
+}
