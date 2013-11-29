a94f46dd59b77aaf640cc97af7c900f23b97d776
frank3b <elverb@gmail.com>
2013-11-29 14:57:35 -0500

    a

--- js/vinculaciones.js
+++ js/vinculaciones.js
@@ -668,13 +668,13 @@ function tomarFoto(){
 			
 			//alert(JSON.stringify(imageData));
 			//alert(imageData);
-			var item = "<li><a href=\"#\" onclick=\"verFoto("+ imageData +")\" data-rel=\"popup\" data-position-to=\"window\"> "+
+			var item = "<li><a href=\"#\" onclick=\"verFoto("+ imageData +");\" data-rel=\"popup\" data-position-to=\"window\"> "+
 			"<img id='img' " + idImagen + " src='"+ imageData +"'/> " +
 			//"<h3>Nombre Imagen</h3>" +
 			//"<p><strong>Cedula:</strong> "+ obj.cliente.cedula +"</p>" +
 			//"<p class=\"ui-li-aside\"><strong>"+ obj.cliente.estado +"</strong></p>" +
 			"</a></li>";
-			//alert(item);
+			alert(item);
 			$('#listaAnexos').append(item).listview('refresh');
 			//$('#img' + idImagen).attr('src', "data:image/jpeg;base64," + imageData);
 			
